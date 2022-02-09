2b4cfae5f0affc193f4ca5a05bad67724b7cd3de
areding <areding@gmail.com>
2022-02-09 17:11:03 +0800

    updated GammaGamma with student question

--- _build/jupyter_execute/Unit4-GammaGamma.py
+++ _build/jupyter_execute/Unit4-GammaGamma.py
@@ -10,6 +10,7 @@ import numpy as np
 from scipy.special import gamma as gamma_func
 from scipy.optimize import fsolve
 from scipy.stats import gamma
+from tqdm.auto import tqdm
 
 
 # # Gamma Gamma
@@ -23,7 +24,8 @@ from scipy.stats import gamma
 # 
 # These terms don't all mean exactly the same thing. For our class, the main distinction the professor draws is between HPD-credible sets and equitailed credible sets. Sometimes the homework will ask for one or the other, or both, so make sure you understand the difference. If the homework doesn't specify, you can use either, but the equitailed credible set is easier to calculate.
 # 
-# First, here's a direct translation from the MATLAB code.
+# ## Translation of MATLAB code (magic k)
+# First, here's a direct translation from the MATLAB code. The professor didn't include how to find $k(\alpha)$ in this version.
 
 # In[2]:
 
@@ -64,7 +66,7 @@ plt.plot([0.0246, 0.2741], [0, 0], 'r-', linewidth=4)
 plt.show()
 
 
-# In[17]:
+# In[5]:
 
 
 # hpd credible set
@@ -80,7 +82,36 @@ print(f'Probability within hpd bounds: {prob_hpd}')
 print(f'length of hpd cs: {upper_hpd - lower_hpd}')
 
 
-# In[5]:
+# In[ ]:
+
+
+from scipy.optimize import fsolve
+from scipy.stats import gamma
+import numpy as np
+
+# our pdf is Gamma(4, 1/29) where 29 is the rate.
+# looking at the plot, k must be between 0-2
+
+a = 4
+b = 29
+alpha = 0.05
+tolerance = 0.0001
+
+possible_k = np.arange(0.01, 2, tolerance)
+
+for k in tqdm(possible_k):
+    lower = fsolve(lambda x: gamma.pdf(x, a, scale=1 / b) - k, 0.05)[0]
+    upper = fsolve(lambda x: gamma.pdf(x, a, scale=1 / b) - k, 0.4)[0]
+    prob = gamma.cdf(upper, a, scale=1 / b) - gamma.cdf(lower, a, scale=1 / b)
+    if abs(prob - (1 - alpha)) <= tolerance:
+        print(f"{k=}")
+        print(f"HPD credible set: {lower, upper}")
+        print(f"length = {upper - lower}")
+        print(f"probability within these bounds: {prob}")
+        break
+
+
+# In[ ]:
 
 
 # equi-tailed credible set
@@ -95,7 +126,7 @@ print(f'Probability within eqt bounds: {prob_eqt}')
 print(f'length of eqt cs: {upper_eqt - lower_eqt}')
 
 
-# In[6]:
+# In[ ]:
 
 
 # figure 2
@@ -116,3 +147,55 @@ plt.show()
 
 
 # Right now I want to get through these examples so Spring 2022 students can get some use out of them. Later on, I think this example could use a lot more explanation and expansion.
+
+# ## Find the HPD credible interval when you don't know k
+# 
+# A student in the Spring 2022 class was wondering where the .05 and .4 values entered to ```fsolve()``` came from. They're just your guesses for the lower and upper bounds of the credible interval. They give the optimizer a starting point.
+# 
+# That led to the bigger question of where $k$ came from. What you need to find is $k(α)$, which is a horizontal line that intersects the lower and upper bounds of your HPD credible interval. The reason we want a horizontal line is that this ensures that you've got the shortest interval covering credibility $1 - α$ (we usually choose a credibility of .95 in this course, but that's arbitrary).
+# 
+# I don't know how the professor did it originally, but I do know two methods. First, the optimization method, pretty much just trying lots of values for $k$ (guessing Professor Vidakovic did it this way):
+# 
+# 1. Loop over a bunch of possible k values, each time using an equation like fzero() on the pdf of your posterior minus k. This will solve for the location that your pdf intersects with k. You need to feed the optimizer an initial guess, one lower, near the lower bound of the credible interval, and one higher, near the upper bound.
+# 
+# 2. Enter each bound into the posterior CDF, subtract the results to get the probability the posterior falls within those bounds. If it's close enough to 1 - α, you can stop the loop because you have your answer. If not, the loop continues.
+
+# In[ ]:
+
+
+# our pdf is Gamma(4, 1/29) where 29 is the rate.
+# looking at the plot, k must be between 0-2
+
+a = 4
+b = 29
+alpha = 0.05
+tolerance = 0.0001
+
+# initial guesses
+guess_lwr = 0.05
+guess_upr = 0.4
+possible_k = np.arange(0, 2, tolerance)
+
+# if loop finishes without printing anthing, no solution within given tolerance
+# solution: adjust tolerance or initial guesses
+for k in tqdm(possible_k):
+    lower = fsolve(lambda x: gamma.pdf(x, a, scale=1 / b) - k, guess_lwr)[0]
+    upper = fsolve(lambda x: gamma.pdf(x, a, scale=1 / b) - k, guess_upr)[0]
+    prob = gamma.cdf(upper, a, scale=1 / b) - gamma.cdf(lower, a, scale=1 / b)
+    if abs(prob - (1 - alpha)) <= tolerance:
+        print(f"{k=}")
+        print(f"HPD credible set: {lower, upper}")
+        print(f"length = {upper - lower}")
+        print(f"probability within these bounds: {prob}")
+        break
+
+
+# This method is really finicky, you might find yourself needing to adjust the tolerance and initial guesses quite a bit. It also wouldn't work as written for any posterior where the HPD credible set wasn't a continuous interval (as discussed in this post), but it could be extended to work for that situation.
+# 
+# There's another way that I find simpler, which involves sampling lots of values from the posterior. I don't have time to write it up now, but I will add it here sometime soon.
+
+# In[ ]:
+
+
+
+
--- _build/jupyter_execute/Unit6-exponential.py
+++ _build/jupyter_execute/Unit6-exponential.py
@@ -10,44 +10,86 @@ import arviz as az
 import matplotlib.pyplot as plt
 
 
+# In[7]:
+
+
+kicks_cincy = np.array([53, 53, 43, 35, 40, 52, 21, 27, 0, 54, 51, 48, 41, 58, 46, 46, 43, 54, 52])
+kicks_la = np.array([53, 38, 48, 42, 47, 32, 47, 27, 54, 37, 39, 44, 55, 55, 32, 0, 43, 46, 40, 40])
+kicks_both = np.append(kicks_cincy, kicks_la)
+np.median(kicks_both), np.mean(kicks_both), np.std(kicks_both)
+
+
 # # Bartholomew (1957) for Type I censoring
-# 
 # 
 # Adapted from [Codes for Unit 6: exponential1.odc](https://www2.isye.gatech.edu/isye6420/supporting.html).
 # 
 # Associated lecture videos: [Unit 6 Lesson 9](https://www.youtube.com/watch?v=6gKnM6vL8Po&list=PLv0FeK5oXK4l-RdT6DWJj0_upJOG2WKNO&index=60).
 # 
+# Dataset from: [A Problem in Life Testing (Bartholemew 1957)](https://www.jstor.org/stable/2280905?seq=1#metadata_info_tab_contents).
 # 
-# These are the notes from the odc file (I'll clean these up another time):
+# ## Results of a life test on ten pieces of equipment
+# |Item No.|1|2|3|4|5|6|7|8|9|10|
+# |-|-|-|-|-|-|-|-|-|-|-|
+# |Date of installation|11 June|21 June|22 June|2 July|21 July|31 July|31 July|1 Aug.|2 Aug.|10 Aug.|
+# |Date of Failure|13 June| - | 12 Aug.| - |23 Aug.|27 Aug.|14 Aug.|25 Aug.|6 Aug.| - |
+# |Life in Days|2|(119)|51|(77)|33|27|14|24|4|(37)|
+# |No. Days to End of Period|81|72|70|60|41|31|31|30|29|21|
 # 
-# ```
-# all=[2  72*  51  60*  33  27  14  24  4  21*];
-# sum(all) = 308
-# observed=[2   51   33 27 14 24 4  ];
-# sum(observed) =155
-# n=10, r=7
-# ```
-# TAKE CENSORED AS OBSERVED  308/10=30.8
+# The observations above are the result of a test on the lifespan of some piece of equipment. The test had to end on August 31, but items 2, 4, and 10 did not fail by that date (the values in parentheses are the eventual lifespans of those pieces of equipment, but they were not known on August 31). So all we know for the purposes of the experiment is that they worked for 72, 60, and 21 days, respectively, and that they will continue working for some unknown additional time period.
 # 
-# IGNORE CENSORED 155/7 = 22.1429
+# The goal of the experiment is to provide a lifespan estimate. We could:
 # 
-# MLE UNDER EXPONENTIAL MODEL 308/7 = 44
+# 1. Take the censored data as observed:
+#     - Divide the total observed working time (308 days) and divide by the equipment count (10) to get an average lifetime of 308 for an estimate of 30.8 days.
+#     - Problem: underestimates actual average lifespan.
+# 
+# 2. Ignore the equipment that didn't fail:
+#     - Take mean lifetime of the pieces of equipment that broke within the experiment period for an estimate of 22.1 days.
+#     - Problem: selection bias, underestimates even more.
+# 
+# 3. Use the classical method:
+#     - Maximum Likelihood Estimation (MLE) under an exponential model. Total observed lifetime divided by 7 (number of observed failures) for an estimate of 44.0 days.
+#     - Problem: small sample size.
+# 
+# The actual mean lifespan of all pieces of equipment was 38.8 days.
+# 
+# ## Bayesian approach
+# What we need to do is called Type-1 censoring. We will right-censor the three machines that still hadn't failed by August 31.
 
-# In[ ]:
+# In[2]:
 
 
+# gamma dist parameters
 alpha = .01
 beta = .1
 
-observed= (2, NA, 51, NA, 33, 27, 14, 24, 4, NA)
+observed = (2, -1, 51, -1, 33, 27, 14, 24, 4, -1)
 censored = (0, 72, 0, 60, 0, 0, 0, 0, 0, 21)
+
+y = (2, 72, 51, 60, 33, 27, 14, 24, 4, 21)
+
+def censored_exponential(y, alpha, beta):
+    with pm.Model() as model:
+        # prior
+        lam = pm.Gamma('lam', alpha, beta)
+        mu = pm.Deterministic('mu', 1/lam)
+        obs_latent = pm.Exponential.dist(lam)
+        observed = pm.Censored('observed', obs_latent, observed=y)
+        
+    return model
+
+
+# In[5]:
+
+
+with censored_exponential(y, alpha, beta):
+    trace = pm.sample(tune=1000, return_inferencedata=True)
 
 
 # In[ ]:
 
 
-with pm.Model() as bartholomew:
-    
+az.summary(trace, hdi_prob=.95)
 
 
 # BUGS results:
@@ -59,6 +101,28 @@ with pm.Model() as bartholomew:
 # | observed[2]   | 122.8           | 60.45  | 0.2442   | 73.12    | 103.5   | 284.5     | 1001  | 100000 |
 # | observed[4]   | 110.8           | 59.93  | 0.2145   | 61.09    | 91.81   | 269.8     | 1001  | 100000 |
 # | observed[10]  | 72.01           | 60.28  | 0.2155   | 22.13    | 52.89   | 232.6     | 1001  | 100000 |
+
+# In[8]:
+
+
+from importlib.metadata import version
+
+
+# In[9]:
+
+
+version('pymc')
+
+
+# In[ ]:
+
+
+
+
+
+# Notes:
+# 
+# - PyMC 4.0 appears to have a new pm.Censored() function, but it's still in beta and the version on conda-forge didn't have that function available when I was working on this. I'll update later when the first stable release comes out (see [this PyMC example](https://docs.pymc.io/projects/examples/en/latest/generalized_linear_models/GLM-truncated-censored-regression.html#censored-regression-model)).
 
 # In[ ]:
 
